3c427ecb39662b3029014b4ebd6b992f2b5d5647
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-22 17:29:47 +0200

    refactor(core): Made minor refactors

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,6 @@ import 'package:planetary_forecast/models/earth_weather.dart';
 import 'package:planetary_forecast/providers/mars_weather_provider.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -15,18 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _cityController = TextEditingController();
 
   @override
-  void dispose() {
-    _cityController.dispose();
-    super.dispose();
-  }
-
-  void _fetchEarthWeather(BuildContext context) {
-    final provider = Provider.of<EarthWeatherProvider>(context, listen: false);
-    provider.fetchEarthWeather(_cityController.text);
-  }
-
-  void _fetchMarsWeather(BuildContext context) {
-    final provider = Provider.of<MarsWeatherProvider>(context, listen: false);
-    provider.fetchMarsWeather();
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
