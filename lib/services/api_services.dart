b76f77ef378d310ac6c6e63b3cfed637ef489d3c
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-23 11:53:23 +0200

    refactor(core): Deleted planet weather API service and model

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,17 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:planetary_forecast/models/earth_weather.dart';
-import '../models/planet_weather.dart';
 import 'api_key.dart';
 
 class ApiService {
   static const String _openWeatherMapApiKey = openWeatherMapApiKey;
   static const String _openWeatherMapApiUrl =
       'https://api.openweathermap.org/data/2.5/weather';
-
-  static const String _ninjaApiKey = ninjaApiKey;
-  static const String _ninjaApiUrl =
-      'https://api.api-ninjas.com/v1/planets?name=';
 
   static Future<EarthWeather> fetchEarthWeather(String cityName) async {
     final url =
@@ -26,17 +21,4 @@ class ApiService {
     }
   }
 
-  static Future<PlanetWeather> fetchPlanetWeather(String planet) async {
-    final url = '$_ninjaApiUrl$planet';
-    final response = await http.get(
-      Uri.parse(url),
-      headers: {'X-Api-Key': _ninjaApiKey});
-
-    if (response.statusCode == 200) {
-      final json = jsonDecode(response.body);
-      return PlanetWeather.fromJson(json[0]);
-    } else {
-      throw Exception('Failed to fetch $planet weather data');
-    }
-  }
 }
