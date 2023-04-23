a84cacce6007d6a02cf3cd09b46bf6c20f31d592
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-04-23 16:27:58 +0200

    refactor(ui): Disabled debug banner & changed app title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Planetary Forecast',
+      debugShowCheckedModeBanner: false,
+      title: 'WeatherNow',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple
       ),
